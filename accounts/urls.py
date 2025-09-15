a8f2f3564bf9aeff2d2b7f3120b4af60679cc609
vinaysurale <vinaysuraleee@gmail.com>
2025-09-15 22:14:26 +0530

    Fix syntax error in accounts URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,10 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import create_superuser_temp # Add this import
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/login.html'), name='logout'),
-    
+]
