b0699047e3efabb0cc99ab6842707e05c1871370
vinaysurale <vinaysuraleee@gmail.com>
2025-09-25 20:56:22 +0530

    Configure live database connection

--- ridelink_project/settings.py
+++ ridelink_project/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 """
 Django settings for ridelink_project project.
 """
@@ -68,12 +69,12 @@ WSGI_APPLICATION = 'ridelink_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Fallback to sqlite database for local development
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
